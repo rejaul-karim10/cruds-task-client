745e67bf55664530ad0e1f7b7d4caafb6f12140a
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-01-15 02:58:30 +0800

    fetch data from mongodb database

--- src/components/TableData.jsx
+++ src/components/TableData.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import AddModalData from "./Modal/AddModalData";
 
 const TableData = () => {
+  const [hobbies, setHobbies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/hobbies")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          setHobbies(data.data);
+        } else {
+          toast.error(data.error);
+        }
+      })
+      .catch((error) => toast.error(error.message));
+  }, []);
+  console.log(hobbies);
+  
   return (
     <div className="h-[800px] max-w-[1200px] mx-auto overflow-x-auto w-full">
       <table className="table table-compact w-full">
@@ -17,120 +34,34 @@ const TableData = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <input type="checkbox" className="checkbox checkbox-sm mt-2" />
-            <th>1</th>
-            <td>Aryana Grande</td>
-            <td>+60828373767</td>
-            <td>ariyana@grande.com</td>
-            <td>Littel, Schaden and Vandervort</td>
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-xs btn-gray-900">
-                Action
-              </label>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
-              >
-                <button className="btn btn-xs btn-secondary mt-2">
-                  Update
-                </button>
-                <button className="btn btn-xs btn-error mt-2">Delete</button>
-              </ul>
-            </div>
-          </tr>
-
-          <tr>
-            <input type="checkbox" className="checkbox checkbox-sm mt-2" />
-            <th>1</th>
-            <td>Aryana Grande</td>
-            <td>+60828373767</td>
-            <td>ariyana@grande.com</td>
-            <td>Littel, Schaden and Vandervort</td>
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-xs btn-gray-900">
-                Action
-              </label>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
-              >
-                <button className="btn btn-xs btn-secondary mt-2">
-                  Update
-                </button>
-                <button className="btn btn-xs btn-error mt-2">Delete</button>
-              </ul>
-            </div>
-          </tr>
-
-          <tr>
-            <input type="checkbox" className="checkbox checkbox-sm mt-2" />
-            <th>1</th>
-            <td>Aryana Grande</td>
-            <td>+60828373767</td>
-            <td>ariyana@grande.com</td>
-            <td>Littel, Schaden and Vandervort</td>
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-xs btn-gray-900">
-                Action
-              </label>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
-              >
-                <button className="btn btn-xs btn-secondary mt-2">
-                  Update
-                </button>
-                <button className="btn btn-xs btn-error mt-2">Delete</button>
-              </ul>
-            </div>
-          </tr>
-
-          <tr>
-            <input type="checkbox" className="checkbox checkbox-sm mt-2" />
-            <th>1</th>
-            <td>Aryana Grande</td>
-            <td>+60828373767</td>
-            <td>ariyana@grande.com</td>
-            <td>Littel, Schaden and Vandervort</td>
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-xs btn-gray-900">
-                Action
-              </label>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
-              >
-                <button className="btn btn-xs btn-secondary mt-2">
-                  Update
-                </button>
-                <button className="btn btn-xs btn-error mt-2">Delete</button>
-              </ul>
-            </div>
-          </tr>
-
-          <tr>
-            <input type="checkbox" className="checkbox checkbox-sm mt-2" />
-            <th>1</th>
-            <td>Aryana Grande</td>
-            <td>+60828373767</td>
-            <td>ariyana@grande.com</td>
-            <td>Littel, Schaden and Vandervort</td>
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-xs btn-gray-900">
-                Action
-              </label>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
-              >
-                <button className="btn btn-xs btn-secondary mt-2">
-                  Update
-                </button>
-                <button className="btn btn-xs btn-error mt-2">Delete</button>
-              </ul>
-            </div>
-          </tr>
+          {hobbies.map((hobby, i) => {
+            return (
+              <tr>
+                <input type="checkbox" className="checkbox checkbox-sm mt-2" />
+                <th>{i+1}</th>
+                <td>{hobby.name}</td>
+                <td>{hobby.phoneNumber}</td>
+                <td>{hobby.email}</td>
+                <td>{hobby.hobbies}</td>
+                <div className="dropdown">
+                  <label tabIndex={0} className="btn btn-xs btn-gray-900">
+                    Action
+                  </label>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
+                  >
+                    <button className="btn btn-xs btn-secondary mt-2">
+                      Update
+                    </button>
+                    <button className="btn btn-xs btn-error mt-2">
+                      Delete
+                    </button>
+                  </ul>
+                </div>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
       <AddModalData />
